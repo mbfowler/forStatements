0a552a6e73043cbd04540051a6458d6d7ee95283
Matt Fowler <mbfowler@gmail.com>
2023-03-01 17:59:02 -0500

    Add places array and use array method

--- friends.js
+++ friends.js
@@ -5,3 +5,9 @@ console.log(arrays[3]);
 console.log(arrays[4]);
 
 console.log(arrays[0]);
+
+var places = ["Vietnam", "France", "Hawaii", "Ireland", "Australia"];
+
+var newPlaces = places.pop();
+
+console.log(newPlaces);
